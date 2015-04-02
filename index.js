1658b1dbf98db29414ac3421ae5f5e215cc7c235
xubo <xb451@163.com>
2015-04-02 22:24:07 +0800

    x-server
    x-server 0.1.2

--- index.js
+++
@@ -1,7 +0,0 @@
-var server = require('./server/server');
-var route = require('./route/route');
-var handler = require('./handle/handler');
-exports.start = function() {
-  server.start(route, handler);
-}
-exports.handler = handler;
